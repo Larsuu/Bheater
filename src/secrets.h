346a5e33ba28366f7c2392616ebf46e4b2feb5f9
Lars U <35381274+Larsuu@users.noreply.github.com>
2023-11-07 19:35:58 +0200

    Delete src/secrets.h
    accident

--- src/secrets.h
+++
@@ -1,9 +0,0 @@
-#define WIFINAME "Pihalla"
-#define WIFIPASS "10209997"
-#define MQTTSRV "192.168.1.150"
-
-#define HOSTIP '192, 168, 1, 203'
-#define GAYWAY '192, 168, 1, 1'
-#define SUBNET '255, 255, 255, 0'
-#define DNS '192, 168, 1, 150'
-
